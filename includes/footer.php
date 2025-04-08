6a53f1d7c2535952bdd26e01784c08889c19df8e
Arthur Willers <arthur2008willers@gmail.com>
2025-04-08 12:26:37 -0300

    Add footer component with project information and links

---
+++ includes/footer.php
@@ -0,0 +1,17 @@
+<footer class="bg-tertiary border-top py-2 fixed-bottom">
+  <div class="container d-flex justify-content-center align-items-center gap-3">
+    <span>
+      Desenvolvido por: 
+      <a href="https://github.com/ArthurWillers" target="_blank" class="text-decoration-none">
+        <i class="bi bi-github"></i> Arthur Vinicius Willers
+      </a>
+    </span>
+    <span>
+      Repositório do projeto:
+      <a href="https://github.com/ArthurWillers/NevoaCore" target="_blank" class="text-decoration-none">
+        <i class="bi bi-code-slash"></i> NevoaCore
+      </a>
+    </span>
+    <span class="mb-0">© 2025 NevoaCore. Todos os direitos reservados.</span>
+  </div>
+</footer>
