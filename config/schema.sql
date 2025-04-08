86175fa7ea267745c1ddb27f1b139ef1882b01cf
Arthur Willers <arthur2008willers@gmail.com>
2025-04-08 16:40:49 -0300

    Add initial schema.sql for database setup with user and verification_code tables

---
+++ config/schema.sql
@@ -0,0 +1,90 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.2-1.fc41
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 08, 2025 at 07:39 PM
+-- Server version: 10.11.10-MariaDB
+-- PHP Version: 8.3.19
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `NevoaCore`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `email` varchar(255) NOT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  `password` char(128) DEFAULT NULL,
+  `role` set('admin','user') DEFAULT 'user',
+  `profile_image` varchar(255) DEFAULT 'default_img',
+  `created_at` timestamp NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification_code`
+--
+
+CREATE TABLE `verification_code` (
+  `code` char(8) NOT NULL,
+  `created_at` timestamp NULL DEFAULT current_timestamp(),
+  `fk_user_email` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Indexes for table `verification_code`
+--
+ALTER TABLE `verification_code`
+  ADD PRIMARY KEY (`code`),
+  ADD KEY `fk_verification_user` (`fk_user_email`);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `verification_code`
+--
+ALTER TABLE `verification_code`
+  ADD CONSTRAINT `fk_verification_user` FOREIGN KEY (`fk_user_email`) REFERENCES `user` (`email`) ON DELETE CASCADE;
+
+DELIMITER $$
+--
+-- Events
+--
+CREATE DEFINER=`root`@`localhost` EVENT `clear_expired_codes` ON SCHEDULE EVERY 1 MINUTE STARTS '2025-04-08 16:39:45' ON COMPLETION NOT PRESERVE ENABLE DO DELETE FROM verification_code
+  WHERE created_at < (NOW() - INTERVAL 3 MINUTE)$$
+
+DELIMITER ;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
