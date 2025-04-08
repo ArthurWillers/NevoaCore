dd40ce3a55ff9679cba30bf51fc028771db5634f
Arthur Willers <arthur2008willers@gmail.com>
2025-04-08 10:58:34 -0300

    Add initial HTML structure for NevoaCore application

---
+++ index.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/x-icon" href="./assets/img/nevoa_logo.png">
+	<title>NevoaCore</title>
+	<?php include './includes/bootstrap_styles.php' ?>
+</head>
+
+<body>
+
+
+
+
+	<?php include './includes/bootstrap_script.php' ?>
+</body>
+
+</html>
