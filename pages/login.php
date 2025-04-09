eae3f6b98e4844a95d779103d1e42ee919f28275
Arthur Willers <arthur2008willers@gmail.com>
2025-04-08 21:49:38 -0300

    Add login and register pages with navigation and toast message functionality

---
+++ pages/login.php
@@ -0,0 +1,41 @@
+<?php session_start(); ?>
+
+<!doctype html>
+<html lang="pt-BR">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/x-icon" href="../assets/img/nevoa_logo.png">
+  <?php include '../includes/bootstrap_styles.php' ?>
+  <link rel="stylesheet" href="../assets/css/bootstrap_custom.css">
+  <title>Login</title>
+</head>
+
+<body>
+  <?php include '../includes/toast.php' ?>
+
+  <nav class="navbar navbar-expand-lg border-bottom bg-body-tertiary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">
+        <img src="../assets/img/nevoa_logo.png" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+        NévoaCore
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0 d-flex gap-2">
+          <li class="nav-item">
+            <a class="btn btn-outline-primary" href="../index.php">Voltar</a>
+          </li>
+        </ul>
+      </div>
+  </nav>
+
+
+  <?php include '../includes/footer.php' ?>
+  <?php include '../includes/bootstrap_script.php' ?>
+</body>
+
+</html>
